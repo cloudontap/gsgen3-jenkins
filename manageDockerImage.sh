4e4d0de5bbe5d8e72a9c1a56108d43c7f949032c
Michael Leditschke <mike@ammd.com.au>
2016-05-20 17:08:11 +1000

    Use the terminology used by docker

--- manageDockerImage.sh
+++ manageDockerImage.sh
@@ -8,18 +8,18 @@ REMOTE_TAG_DEFAULT="latest"
 IMAGE_SOURCE_DEFAULT="remote"
 function usage() {
     echo -e "\nManage docker images"
-    echo -e "\nUsage: $(basename $0) -c -i REMOTE_IMAGE -l LOCAL_IMAGE -r REMOTE_TAG -s IMAGE_SOURCE -t LOCAL_TAG"
+    echo -e "\nUsage: $(basename $0) -c -i REMOTE_REPO -l LOCAL_REPO -r REMOTE_TAG -s IMAGE_SOURCE -t LOCAL_TAG"
     echo -e "\nwhere\n"
     echo -e "(o) -c only check if image present, don't try and pull it if not"
     echo -e "    -h shows this text"
-    echo -e "(o) -i REMOTE_IMAGE to use when pulling in the image"
-    echo -e "(o) -l LOCAL_IMAGE to use when saving image in the registry"
+    echo -e "(o) -i REMOTE_REPO to use when pulling in the image"
+    echo -e "(o) -l LOCAL_REPO to use when saving image"
     echo -e "(o) -r REMOTE_TAG to use when pulling in the image"
     echo -e "(o) -s IMAGE_SOURCE is the location to pull from"
-    echo -e "(o) -t LOCAL_TAG to use when saving image in the registry"
+    echo -e "(o) -t LOCAL_TAG to use when saving image"
     echo -e "\nDEFAULTS:\n"
     echo -e "REMOTE_TAG=${REMOTE_TAG_DEFAULT}"
-    echo -e "LOCAL_IMAGE=REMOTE_IMAGE"
+    echo -e "LOCAL_REPO=REMOTE_REPO"
     echo -e "LOCAL_TAG=REMOTE_TAG"
     echo -e "IMAGE_SOURCE=${IMAGE_SOURCE_DEFAULT}"
     echo -e "\nNOTES:\n"
@@ -40,10 +40,10 @@ while getopts ":chr:t:" opt; do
             usage
             ;;
         i)
-            REMOTE_IMAGE="${OPTARG}"
+            REMOTE_REPO="${OPTARG}"
             ;;
         l)
-            LOCAL_IMAGE="${OPTARG}"
+            LOCAL_REPO="${OPTARG}"
             ;;
         r)
             REMOTE_TAG="${OPTARG}"
@@ -63,8 +63,8 @@ while getopts ":chr:t:" opt; do
 done
 
 # Ensure the IMAGE has been provided
-if [[ "${REMOTE_IMAGE}" == "" ]]; then
-	echo "Job requires the remote image name"
+if [[ "${REMOTE_REPO}" == "" ]]; then
+	echo "Job requires the remote repository name"
     RESULT=1
     exit
 fi
@@ -78,18 +78,18 @@ if [[ "${IMAGE_SOURCE}" == "" ]]; then
 fi
 
 # Confirm local image settings
-if [[ "${LOCAL_IMAGE}" == "" ]]; then
-    LOCAL_IMAGE="${REMOTE_IMAGE}"
+if [[ "${LOCAL_REPO}" == "" ]]; then
+    LOCAL_REPO="${REMOTE_REPO}"
 fi
 if [[ "${LOCAL_TAG}" == "" ]]; then
     LOCAL_TAG="${REMOTE_TAG}"
 fi
 
 # Formulate the remote image details
-REMOTE_REGISTRY_IMAGE="${REMOTE_IMAGE}:${REMOTE_TAG}"
-FULL_REMOTE_REGISTRY_IMAGE="${REMOTE_REGISTRY_IMAGE}"
+REMOTE_REPOSITORY="${REMOTE_REPO}:${REMOTE_TAG}"
+FULL_REMOTE_REPOSITORY="${REMOTE_REPOSITORY}"
 if [[ "${SOURCE}" == "remote" ]]; then
-    FULL_REMOTE_REGISTRY_IMAGE="${REMOTE_DOCKER_REGISTRY}/${REMOTE_REGISTRY_IMAGE}"
+    FULL_REMOTE_REPOSITORY="${REMOTE_DOCKER_REGISTRY}/${REMOTE_REPOSITORY}"
     sudo docker login -u ${REMOTE_DOCKER_USER} -p ${REMOTE_DOCKER_PASS} -e ${REMOTE_DOCKER_EMAIL} ${REMOTE_DOCKER_REGISTRY}
     RESULT=$?
     if [[ "$RESULT" -ne 0 ]]; then  
@@ -99,8 +99,8 @@ if [[ "${SOURCE}" == "remote" ]]; then
 fi
 
 # Formulate the local image details
-LOCAL_REGISTRY_IMAGE="${LOCAL_IMAGE}:${LOCAL_TAG}"
-FULL_LOCAL_REGISTRY_IMAGE="${DOCKER_REGISTRY}/${LOCAL_REGISTRY_IMAGE}"
+LOCAL_REPOSITORY="${LOCAL_REPO}:${LOCAL_TAG}"
+FULL_LOCAL_REPOSITORY="${DOCKER_REGISTRY}/${LOCAL_REPOSITORY}"
 
 # Check if image has already been pulled
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASS} -e ${DOCKER_EMAIL} ${DOCKER_REGISTRY}
@@ -109,37 +109,37 @@ if [[ "$RESULT" -ne 0 ]]; then
    echo "Can't log in to ${DOCKER_REGISTRY}"
    exit
 fi
-sudo docker pull ${FULL_LOCAL_REGISTRY_IMAGE}
+sudo docker pull ${FULL_LOCAL_REPOSITORY}
 RESULT=$?
 if [[ "$RESULT" -eq 0 ]]; then  
-	echo "Image ${LOCAL_REGISTRY_IMAGE} present in the registry."
+	echo "Image ${LOCAL_REPOSITORY} present in the registry."
 else
     if [[ "${PULL_IF_ABSENT}" == "true" ]]; then
-        sudo docker pull ${FULL_REMOTE_REGISTRY_IMAGE}
+        sudo docker pull ${FULL_REMOTE_REPOSITORY}
         RESULT=$?
         if [[ "$RESULT" -ne 0 ]]; then
-            echo "Pull of ${REMOTE_REGISTRY_IMAGE} from ${REMOTE_DOCKER_REGISTRY} failed"
+            echo "Pull of ${REMOTE_REPOSITORY} from ${REMOTE_DOCKER_REGISTRY} failed"
         else
             # Tag the image ready to push to the registry
-            sudo docker tag ${FULL_REMOTE_REGISTRY_IMAGE} ${FULL_LOCAL_REGISTRY_IMAGE}
+            sudo docker tag ${FULL_REMOTE_REPOSITORY} ${FULL_LOCAL_REPOSITORY}
 
             # Push to registry
-            sudo docker push ${FULL_LOCAL_REGISTRY_IMAGE}
+            sudo docker push ${FULL_LOCAL_REPOSITORY}
             RESULT=$?
             if [[ "$?" -ne 0 ]]; then
-                echo "Couldn't push image ${LOCAL_REGISTRY_IMAGE} to ${FULL_LOCAL_REGISTRY_IMAGE}"
+                echo "Couldn't push image ${LOCAL_REPOSITORY} to ${FULL_LOCAL_REPOSITORY}"
             fi
         fi
     fi
 fi
 
 if [[ "${PULL_IF_ABSENT}" == "true" ]]; then
-    IMAGEID=$(sudo docker images | grep "${REMOTE_IMAGE}" | grep "${REMOTE_TAG}" | head -1 |awk '{print($3)}')
+    IMAGEID=$(sudo docker images | grep "${REMOTE_REPO}" | grep "${REMOTE_TAG}" | head -1 |awk '{print($3)}')
     if [[ "${IMAGEID}" != "" ]]; then
         sudo docker rmi -f ${IMAGEID}
     fi
 fi
-IMAGEID=$(sudo docker images | grep "${LOCAL_IMAGE}" | grep "${LOCAL_TAG}" | head -1 |awk '{print($3)}')
+IMAGEID=$(sudo docker images | grep "${LOCAL_REPO}" | grep "${LOCAL_TAG}" | head -1 |awk '{print($3)}')
 if [[ "${IMAGEID}" != "" ]]; then
     sudo docker rmi -f ${IMAGEID}
 fi
