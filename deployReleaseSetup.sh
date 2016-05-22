a4a037c083a7046f212efc3fd559cf5f565e8f1a
Michael Leditschke <mike@ammd.com.au>
2016-05-22 15:45:13 +1000

    DEPLOY_TAG -> DEPLOYMENT_TAG. It is now set up in setContext.sh

--- deployReleaseSetup.sh
+++ deployReleaseSetup.sh
@@ -4,8 +4,6 @@ if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 
 trap 'exit ${RESULT:-0}' EXIT SIGHUP SIGINT SIGTERM
 
-. ${GSGEN_JENKINS}/setContext.sh
-
 # Ensure DEPLOYMENT_NUMBER have been provided
 if [[ "${DEPLOYMENT_NUMBER}" == "" ]]; then
 	echo "Job requires the deployment number, exiting..."
@@ -13,11 +11,9 @@ if [[ "${DEPLOYMENT_NUMBER}" == "" ]]; then
     exit
 fi
 
-# Generate the deployment tag
-export DEPLOY_TAG="d${DEPLOYMENT_NUMBER}-${ENVIRONMENT}"
-echo "DEPLOY_TAG=${DEPLOY_TAG}" >> ${WORKSPACE}/context.properties
+. ${GSGEN_JENKINS}/setContext.sh
 
-# Construct the tree based on the deployment tags
-${GSGEN_JENKINS}/constructTree.sh -c ${DEPLOY_TAG} -i ${DEPLOY_TAG}
+# Construct the tree based on the deployment tag
+${GSGEN_JENKINS}/constructTree.sh -c ${DEPLOYMENT_TAG} -i ${DEPLOYMENT_TAG}
 RESULT=$?
 
