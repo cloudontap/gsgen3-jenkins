ba94aa9e5c9d87c26b782b390b41316f6d688aca
Michael Leditschke <mike@ammd.com.au>
2016-05-22 15:40:17 +1000

    Rename a few context variables to be more consistent

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -4,9 +4,9 @@ if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 
 trap 'exit ${RESULT:-0}' EXIT SIGHUP SIGINT SIGTERM
 
-# Generate the deployment tag
-DEPLOY_TAG="d${BUILD_NUMBER}-${ENVIRONMENT}"
-DEPLOY_MESSAGE="Prepare deployment ${DEPLOY_TAG} for ${PROJECT}/${ENVIRONMENT}, based on ${CODE_TAG} (${GIT_COMMIT_SHORT}) of the code"
+# Add deployment number to details
+DETAIL_MESSAGE="Deployment ${DEPLOYMENT_TAG}, ${DETAIL_MESSAGE}"
+echo "DETAIL_MESSAGE=${DETAIL_MESSAGE}" >> ${WORKSPACE}/context.properties
 
 # Process the config repo
 cd ${WORKSPACE}/${OAID}/config/${PROJECT}
@@ -16,10 +16,10 @@ git config user.name  "${GIT_USER}"
 git config user.email "${GIT_EMAIL}"
 
 # Ensure build.ref aligns with the requested code tag
-BUILD_REFERENCE=$(echo -n "${GIT_COMMIT} ${CODE_TAG}")
+BUILD_REFERENCE=$(echo -n "${CODE_COMMIT} ${CODE_TAG}")
 if [[ "$(cat deployments/${ENVIRONMENT}/${BUILD_SLICE}/build.ref)" != "${BUILD_REFERENCE}" ]]; then
 	echo -n "${BUILD_REFERENCE}" > deployments/${ENVIRONMENT}/${BUILD_SLICE}/build.ref
-	git add *; git commit -m "${DEPLOY_MESSAGE}"
+	git add *; git commit -m "${DETAIL_MESSAGE}"
 	RESULT=$?
 	if [[ ${RESULT} -ne 0 ]]; then
 		echo "Can't commit the build reference to the config repo, exiting..."
@@ -32,7 +32,7 @@ BIN_DIR="${WORKSPACE}/${OAID}/config/bin"
 cd solutions/${ENVIRONMENT}
 
 for SLICE in ${SLICE_LIST}; do
-	${BIN_DIR}/createApplicationTemplate.sh -c ${DEPLOY_TAG} -s ${SLICE}
+	${BIN_DIR}/createApplicationTemplate.sh -c ${DEPLOYMENT_TAG} -s ${SLICE}
 	RESULT=$?
 	if [[ ${RESULT} -ne 0 ]]; then
  		echo "Can't generate the template for the ${SLICE} application slice, exiting..."
@@ -41,8 +41,8 @@ for SLICE in ${SLICE_LIST}; do
 done
 
 # All ok so tag the config repo
-echo "Adding tag \"${DEPLOY_TAG}\" to the config repo..."
-git tag -a ${DEPLOY_TAG} -m "${DEPLOY_MESSAGE}"
+echo "Adding tag \"${DEPLOYMENT_TAG}\" to the config repo..."
+git tag -a ${DEPLOYMENT_TAG} -m "${DETAIL_MESSAGE}"
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
 	echo "Can't tag the config repo, exiting..."
@@ -64,7 +64,7 @@ git config user.email "${GIT_EMAIL}"
 
 # Commit the generated application templates
 echo "Committing application templates to the infrastructure repo..."
-git add *; git commit -m "${DEPLOY_MESSAGE}"
+git add *; git commit -m "${DETAIL_MESSAGE}"
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
 	echo "Can't commit the application templates to the infrastructure repo, exiting..."
@@ -72,8 +72,8 @@ if [[ ${RESULT} -ne 0 ]]; then
 fi
 
 # Tag the infrastructure repo
-echo "Adding tag \"${DEPLOY_TAG}\" to the infrastructure repo..."
-git tag -a ${DEPLOY_TAG} -m "${DEPLOY_MESSAGE}"
+echo "Adding tag \"${DEPLOYMENT_TAG}\" to the infrastructure repo..."
+git tag -a ${DEPLOYMENT_TAG} -m "${DETAIL_MESSAGE}"
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
 	echo "Can't tag the infrastructure repo, exiting..."
