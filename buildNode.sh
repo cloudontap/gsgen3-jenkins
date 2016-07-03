808c754d4e3980b54f1bb0141024a35eea2d7f19
Michael Leditschke <ml019.ammd.com.au>
2016-07-03 20:56:35 +1000

    Correct local for detecting existing docker image

--- buildNode.sh
+++ buildNode.sh
@@ -24,8 +24,7 @@ if [[ -f Dockerfile ]]; then
         fi
     fi
     
-    FULL_IMAGE="${DOCKER_REGISTRY}/${REMOTE_REPO}"
-    ${GSGEN_JENKINS}/manageDockerImage.sh -c
+    ${GSGEN_JENKINS}/manageDockerImage.sh -c -s local -i ${REMOTE_REPO}
     RESULT=$?
     if [[ "${RESULT}" -eq 0 ]]; then
         echo "Image ${REMOTE_REPO} already exists"
