c97e6101d29cea73f3403a79bfccb1a5f0c79fd9
Michael Leditschke <mike@ammd.com.au>
2016-07-06 11:52:11 +1000

    Support the ability to set more than one env variable for a task

--- runTask.sh
+++ runTask.sh
@@ -7,8 +7,20 @@ trap 'exit ${RESULT:-0}' EXIT SIGHUP SIGINT SIGTERM
 BIN_DIR="${WORKSPACE}/${OAID}/config/bin"
 cd ${WORKSPACE}/${OAID}/config/${PROJECT}/solutions/${SEGMENT}
 
+# Build up the additional enviroment variables required
+ENVS=""
+for i in "" $(seq 2 20); do
+    ENV_NAME="TASK_ENV${i}"
+    ENV_VALUE="TASK_VALUE${i}"
+    if [[ -n "${!ENV_NAME}" ]]; then
+        ENVS="${ENVS} -e ${!ENV_NAME} -v \"${!ENV_VALUE}\""
+    else
+        break
+    fi 
+done
+
 # Create the required task
-${BIN_DIR}/runTask.sh -t "${TASK_TIER}" -i "${TASK_COMPONENT}" -w "${TASK_NAME}" -e "${TASK_ENV}" -v "${TASK_COMMAND}" 
+${BIN_DIR}/runTask.sh -t "${TASK_TIER}" -i "${TASK_COMPONENT}" -w "${TASK_NAME}" ${ENVS}
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
 	echo "Running of task failed, exiting..."
