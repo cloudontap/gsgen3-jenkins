d30b184d53156071c5c2db046111a4006814c14e
Michael Leditschke <mike@ammd.com.au>
2016-07-07 12:45:28 +1000

    Use arrays to manage run parameters to get around env variable value quoting issues

--- runTask.sh
+++ runTask.sh
@@ -8,21 +8,21 @@ BIN_DIR="${WORKSPACE}/${OAID}/config/bin"
 cd ${WORKSPACE}/${OAID}/config/${PROJECT}/solutions/${SEGMENT}
 
 # Build up the additional enviroment variables required
-ENVS=""
+ENVS=()
 for i in "" $(seq 2 20); do
     ENV_NAME="TASK_ENV${i}"
     ENV_VALUE="TASK_VALUE${i}"
     if [[ -n "${!ENV_NAME}" ]]; then
         if [[ -n "${GSGEN_DEBUG}" ]]; then echo ${!ENV_NAME}; fi
         if [[ -n "${GSGEN_DEBUG}" ]]; then echo ${!ENV_VALUE}; fi
-        ENVS="${ENVS} -e ${!ENV_NAME} -v \"${!ENV_VALUE}\""
+        ENVS+=( "${!ENV_NAME}", "${!ENV_VALUE}")
     fi 
 done
 
 if [[ -n "${GSGEN_DEBUG}" ]]; then echo ${ENVS}; fi
 
 # Create the required task
-${BIN_DIR}/runTask.sh -t "${TASK_TIER}" -i "${TASK_COMPONENT}" -w "${TASK}" ${ENVS}
+${BIN_DIR}/runTask.sh -t "${TASK_TIER}" -i "${TASK_COMPONENT}" -w "${TASK}" ${ENVS[*]}
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
 	echo "Running of task failed, exiting..."
