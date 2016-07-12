80adb5c035d6c4a1b0185fa4bf506466a0b41c0b
Michael Leditschke <mike@ammd.com.au>
2016-07-12 13:27:42 +1000

    Avoid having to pull in a complete code repo to confirm the presence of a tag

--- confirmBuild.sh
+++ confirmBuild.sh
@@ -5,31 +5,30 @@ if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
 # Ensure CODE_TAG have been provided
-if [[ "${CODE_TAG}" == "" ]]; then
+if [[ -z "${CODE_TAG}" ]]; then
 	echo "Job requires the code tag, exiting..."
     exit
 fi
 
-# Pull in the code repo
-git clone https://${GITHUB_USER}:${GITHUB_PASS}@${PROJECT_CODE_REPO} -b ${CODE_BRANCH} code
-RESULT=$?
-if [[ ${RESULT} -ne 0 ]]; then
- 	echo "Can't fetch the code repo, exiting..."
+# Get the commit corresponding to the tag
+TAG_COMMIT=$(git ls-remote -t https://${GITHUB_USER}:${GITHUB_PASS}@${PROJECT_CODE_REPO} "${CODE_TAG}" | cut -f 1)
+CODE_COMMIT=$(git ls-remote -t https://${GITHUB_USER}:${GITHUB_PASS}@${PROJECT_CODE_REPO} "${CODE_TAG}^{}" | cut -f 1)
+if [[ -z "${CODE_COMMIT}" ]]; then
+    echo "Code tag not found in the code repo, exiting..."
  	exit
 fi
-cd code
 
-# Confirm the tag is present
-CODE_TAG_MESSAGE=$(git tag -l ${CODE_TAG} -n1 | tr -s " " | cut -d " " -f 2-)
-if [[ "${CODE_TAG_MESSAGE}" == "" ]]; then
-	echo "Code tag not found in the code repo, exiting..."
-   	RESULT=1
+# Fetch other info about the tag
+# We are using a github api here to avoid having to pull in the whole repo - 
+# git currently doesn't have a command to query the message of a remote tag
+CODE_COMMIT_SHORT="${CODE_COMMIT:0:8}"
+PROJECT_CODE_REPO_BASE="${PROJECT_CODE_REPO#*/}"
+PROJECT_CODE_REPO_BASE="${PROJECT_CODE_REPO_BASE%.git}"
+CODE_TAG_MESSAGE=$(curl https://${GITHUB_USER}:${GITHUB_PASS}@api.github.com/repos/${PROJECT_CODE_REPO_BASE}/tags/${TAG_COMMIT} | jq .message | tr -d '"')
+if [[ -z "${CODE_TAG_MESSAGE}" ]]; then
+	echo "Tag message not found in the code repo, exiting..."
 	exit
 fi
-
-# Determine the commit matching the tag in the code repo 
-CODE_COMMIT=$(git rev-list -n 1 ${CODE_TAG})
-CODE_COMMIT_SHORT=$(git rev-list -n 1 --abbrev-commit ${CODE_TAG})
 
 # Details of job
 DETAIL_MESSAGE="${DETAIL_MESSAGE}, code=${CODE_TAG} (${CODE_COMMIT_SHORT})"
@@ -46,8 +45,9 @@ if [[ (-z "${SLICE}" ) || ( -n "${DOCKER_INHIBIT_SLICE_IN_REPO}" ) ]]; then
 else
     export REMOTE_REPO="${PROJECT}/${SLICE}-${CODE_COMMIT}"
 fi
-${GSGEN_JENKINS}/manageDockerImage.sh -c -s local -i ${REMOTE_REPO}
+${GSGEN_JENKINS}/manageDockerImage.sh -c -i ${REMOTE_REPO}
 RESULT=$?
 if [[ "${RESULT}" -ne 0 ]]; then
     echo "Image ${REMOTE_REPO} not found. Was the build successful?"
+    exit
 fi
