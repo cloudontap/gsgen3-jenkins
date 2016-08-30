c9b259a947dc599ad3ec7833eb05ae04ba425757
Michael Leditschke <ml019@ammd.com.au>
2016-08-30 10:57:18 +1000

    Default return code indicates an error

--- manageEnvironment.sh
+++ manageEnvironment.sh
@@ -2,7 +2,7 @@
 
 if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 
-trap 'exit ${RESULT:-0}' EXIT SIGHUP SIGINT SIGTERM
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
 # Generate the deployment template for the required slice
 BIN_DIR="${WORKSPACE}/${AID}/config/bin"
