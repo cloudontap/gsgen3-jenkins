7696af5d4dc55a3a1509706f8d325c423eea5468
Michael Leditschke <ml019@ammd.com.au>
2016-10-31 11:37:37 +1100

    Force automation user id to upper case

--- setContext.sh
+++ setContext.sh
@@ -130,8 +130,8 @@ if [[ (-n ${!AID_AWS_ACCOUNT_ID_VAR}) && (-n ${!AID_AUTOMATION_USER_VAR}) ]]; th
     # Assume automation role using automation user access credentials
     # Note that the value for the user is just a way to obtain the access credentials
     # and doesn't have to be the same as the IAM user name
-    AID_AWS_ACCESS_KEY_ID_VAR="${!AID_AUTOMATION_USER_VAR}_AWS_ACCESS_KEY_ID"
-    AID_AWS_SECRET_ACCESS_KEY_VAR="${!AID_AUTOMATION_USER_VAR}_AWS_SECRET_ACCESS_KEY"
+    AID_AWS_ACCESS_KEY_ID_VAR="${!AID_AUTOMATION_USER_VAR^^}_AWS_ACCESS_KEY_ID"
+    AID_AWS_SECRET_ACCESS_KEY_VAR="${!AID_AUTOMATION_USER_VAR^^}_AWS_SECRET_ACCESS_KEY"
     export AWS_ACCESS_KEY_ID="${!AID_AWS_ACCESS_KEY_ID_VAR}"
     export AWS_SECRET_ACCESS_KEY="${!AID_AWS_SECRET_ACCESS_KEY_VAR}"
     TEMP_CREDENTIAL_FILE="$WORKSPACE/temp_aws_credentials.json"
