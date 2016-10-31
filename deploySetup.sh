9b006726c9f93bbdd1d516270841eeaab6627a0c
Michael Leditschke <ml019@ammd.com.au>
2016-10-31 11:57:27 +1100

    Correct logic for including build reference in the detail message

--- deploySetup.sh
+++ deploySetup.sh
@@ -8,7 +8,8 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 cd ${WORKSPACE}/${AID}/config/${PRODUCT}
 BUILD_FILE="appsettings/${SEGMENT}/${SLICE}/build.ref"
 if [[ -e ${BUILD_FILE} ]]; then
-    echo "BUILD_REFERENCE=$(cat ${BUILD_FILE})" >> ${WORKSPACE}/context.properties
+    BUILD_REFERENCE="$(cat ${BUILD_FILE})"
+    echo "BUILD_REFERENCE=${BUILD_REFERENCE}" >> ${WORKSPACE}/context.properties
 fi
 
 # Generate the notification message information
