558616a233b95b23a39a2dc21c09c66cd163cfbb
Michael Leditschke <ml019@ammd.com.au>
2016-11-26 21:28:42 +1100

    Shift detail update from parameters script to setup script

--- prepareReleaseSetup.sh
+++ prepareReleaseSetup.sh
@@ -3,8 +3,6 @@
 if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
-# No parameters at this stage but we need to update the DETAIL_MESSAGE
-
 # Add release number to details
 DETAIL_MESSAGE="release=${RELEASE_TAG}, ${DETAIL_MESSAGE}"
 echo "DETAIL_MESSAGE=${DETAIL_MESSAGE}" >> ${AUTOMATION_DATA_DIR}/context.properties
